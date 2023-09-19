728cb5fe708fc8f744a66d84ce1e36984eb6c0e0
Rohit Jagadale <91559740+rohitjagadale@users.noreply.github.com>
2023-09-19 15:30:04 +0530

    Delete Day wise lab assignments /OS/ifelse.sh

--- Day wise lab assignments /OS/ifelse.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-read -p "Enter the First Number" num1
-read -p "Enter the Second Number" num2
-if [ $num1 -eq $num2 ]
-then
-echo "Number are Equal"
-else
-echo "Number are Not Equal"
-fi
